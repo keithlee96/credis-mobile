99fcad417582a7b72c24d23f2c6546f02c40fb9a
Keith <keithsl@cre-mm.mobility.unimelb.net.au>
2017-12-11 13:22:11 +1100

    Initial commit

---
+++ platforms/ios/build/credismobile.build/Debug-iphonesimulator/credismobile.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/keithsl/vueProjects/credis-mobile/platforms/ios/credismobile/credismobile-Prefix.pch \
+  /Users/keithsl/vueProjects/credis-mobile/platforms/ios/internal/main.m \
+  /Users/keithsl/vueProjects/credis-mobile/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/keithsl/vueProjects/credis-mobile/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/keithsl/vueProjects/credis-mobile/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/keithsl/vueProjects/credis-mobile/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/keithsl/vueProjects/credis-mobile/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/keithsl/vueProjects/credis-mobile/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/keithsl/vueProjects/credis-mobile/platforms/ios/internal/TNSDebugging.h \
+  /Users/keithsl/vueProjects/credis-mobile/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
